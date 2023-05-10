7a855866c6c2ccfdd2b85fc3766a719fa80a9e96
Giovanni Jecha <giovanni.jecha@studenti.isissgobetti.it>
2023-05-10 09:31:08 +0200

    test

--- Esercizio1/ImportFileJSON.js
+++
@@ -1,2 +0,0 @@
-let obj = require('./src/Persone.json');
-console.log(obj[0].person.name); 
---
+++ Esercizio1/src/ImportFileJSON.js
@@ -0,0 +1,2 @@
+let obj = require('/home/jecha/code/EserciziNode/esercizio1/Esercizio1/Persone.json');
+console.log(obj[0].person.name); 
