e2dd0c862eff72e89d2468a790bb58ba9a663fda
Giovanni Jecha <giovanni.jecha@studenti.isissgobetti.it>
2023-05-21 19:16:19 +0200

    test

--- Esercizio1/src/Lab024.js
+++ Esercizio1/src/Lab024.js
@@ -1,146 +1,70 @@
-const prompt = require("prompt-sync")();
-
-class GestioneFileSynk {
-    fs = require('fs'); //selezionare libreria
-
-    constructor(nomeFile) {
-        this.nomeFile = nomeFile;
-
-    }
-    ReadFile() {
-        try {
-            const data = this.fs.readFileSync(this.nomeFile, "utf8");
-            return data;
-        } catch (err) {
-            console.error(err);
-        }
-    };
-    WriteFile(msg) {
-        try {
-            this.fs.writeFileSync(this.nomeFile, msg + " \r\n", { flag: 'a+' });
-            // file written successfully
-        } catch (err) {
-            console.error(err);
-        }
-    };
-    AppendFile(msg) {
-        ////?????????????????????
-    };
-}
+const prompt = require('prompt-sync')();
+const fs = require('fs');
 
 class Persona {
-    #nome;
-    #cognome;
-    #data_nascita;
-    constructor(nome, cognome, data_nascita) {
-        this.nome = nome;
-        this.cognome = cognome;
-        this.data_nascita = data_nascita;
-    }
-    set nome(name) {
-        this.#nome = name;
-    }
-    set cognome(surname) {
-        this.#cognome = surname;
-    }
-    set data_nascita(date) {
-        this.#data_nascita = date;
-    }
-    get nome() {
-        return this.#nome;
-    }
-    get cognome() {
-        return this.#cognome;
-    }
-    get data_nascita() {
-        return this.#data_nascita;
-    }
-    ToString() {
-        return `Nome : ${this.nome}\nCognome : ${this.cognome}\nData nascita : ${this.data_nascita}`;
-    }
-    ToCsv() {
-        return `${this.nome},${this.cognome},${this.data_nascita}`;
-    }
+  constructor(nome, cognome, data_nascita) {
+    this.nome = nome;
+    this.cognome = cognome;
+    this.data_nascita = data_nascita;
+  }
+  toString() {
+    return 'Nome : ' + this.nome + '\nCognome : ' + this.cognome + '\nData nascita : ' + this.data_nascita
+  }
 }
 
-function importFile(file, persone) {
-    let gFs = new GestioneFileSynk(file);
-    let data = gFs.ReadFile().split(/\r?\n/);
-    data.splice(0, 1);
-
-    for (let i = 0; i < data.length; i++) {
-        var riga = data[i].split(",");
-        var persona = new Persona(riga[0], riga[1], riga[2]);
-        persone.push(persona);
-    }
-
-    console.log("Il file è importato correttamente");
-
-    for (let i = 0; i < persone.length; i++) {
-        console.log("Persona " + (i + 1) + ":\n");
-        console.log(persone[i].ToString());
-    }
+// Esportazione in formato JSON
+function esporta() {
+  const nome = prompt('Inserisci il nome: ');
+  const cognome = prompt('Inserisci il cognome: ');
+  const data_nascita = prompt('Inserisci la data di nascita: ');
+  const persona = new Persona(nome, cognome, data_nascita);
+  console.log(persona.toString());
+  const jsonContent = JSON.stringify(persona);
+  fs.writeFileSync('persona.json', jsonContent);
 }
 
-function exportFile(file, persone) {
-    let gfsEsporta = new GestioneFileSynk(file);
-    gfsEsporta.WriteFile("Nome,Cognome,data_di_nascita");
-    for (let i = 0; i < persone.length; i++) {
-        gfsEsporta.WriteFile(persone[i].ToCsv());
-    }
-    console.log("File esportati correttamente");
+// Importazione da formato JSON
+function importa() {
+  const jsonImport = fs.readFileSync('persona.json', 'utf8');
+  const importedPersonaData = JSON.parse(jsonImport);
+  const importedPersona = new Persona(importedPersonaData.nome, importedPersonaData.cognome, importedPersonaData.data_nascita);
+  console.log(importedPersona.toString());
 }
 
-function countWord(file, word) {
-    let count = 0;
-    let gFs = new GestioneFileSynk(file);
-    let data = gFs.ReadFile();
-    let righe = data.split(/\r?\n/);
-    righe.splice(0, 1);
-
-    for (let i = 0; i < righe.length; i++) {
-        var riga = righe[i];
-        var parole = riga.split(",");
-        for (let j = 0; j < parole.length; j++) {
-            var parola = parole[j];
-            if (parola == word) {
-                count++;
-            }
-        }
+function contaOccorrenze(parola) {
+  const jsonImport = fs.readFileSync('persona.json', 'utf8');
+  const importedPersonaData = JSON.parse(jsonImport);
+  let conteggio = 0;
+  for (let key in importedPersonaData) {
+    if (importedPersonaData[key] === parola) {
+      conteggio++;
     }
-
-    console.log(`Ci sono ${count} occorrenze della parola ${word}`);
+  }
+  console.log('La parola ' + parola + ' compare ' + conteggio + 'volte nel file JSON.');
 }
 
-while(true) {
-    const persone = [];
-    let controllo = true;
-    while (controllo) {
-        console.log("Seleziona un'opzione:");
-        console.log("1. Importa un file CSV");
-        console.log("2. Esporta un file CSV");
-        console.log("3. Conta le occorrenze di una parola nel file CSV");
-        console.log("4. Esci");
-        const choice = prompt("Scelta: ");
-        switch (parseInt(choice)) {
-            case 1:
-                var file = prompt("Inserisci il percorso del file? ");
-                importFile(file, persone);
-                break;
-            case 2:
-                var file = prompt("Inserisci il percorso del file? ");
-                exportFile(file, persone);
-                break;
-            case 3:
-                var file = prompt("Inserisci il percorso del file? ");
-                var word = prompt("Inserisci la parola da cercare? ");
-                countWord(file, word);
-                break;
-            case 4:
-                console.log("Programma terminato.");
-                process.exit(0);
-            default:
-                console.log("Scelta non valida.");
-        }
+while (true) {
+    console.log("Seleziona un'opzione:");
+    console.log("1. Importa un file CSV");
+    console.log("2. Esporta un file CSV");
+    console.log("3. Conta occorrenze in un file CSV");
+    console.log("0. Esci");
+    const choice = prompt("Scelta: ");
+    switch (choice) {
+      case "1":
+        importa();
+        break;
+      case "2":
+        esporta();
+        break;
+      case "3":
+        const parola = prompt("Inserisci la parola da cercare: ");
+        contaOccorrenze(parola);
+        break;
+      case "0":
+        console.log("Programma terminato.");
+        process.exit(0);
+      default:
+        console.log("Scelta non valida.");
     }
-}
+  }
