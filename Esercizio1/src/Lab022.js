76acc9413a571cd3fc64089674d55d2ab4422d64
Giovanni Jecha <giovanni.jecha@studenti.isissgobetti.it>
2023-05-21 18:36:23 +0200

    commit

--- Esercizio1/src/Lab022.js
+++ Esercizio1/src/Lab022.js
@@ -12,19 +12,44 @@ class Persona {
   }
 }
 
-const nome = prompt('Inserisci il nome: ');
-const cognome = prompt('Inserisci il cognome: ');
-const data_nascita = prompt('Inserisci la data di nascita: ');
-const persona = new Persona(nome, cognome, data_nascita);
-console.log(persona.toString());
-
 // Esportazione in formato CSV
-const csvContent = `nome,cognome,data_nascita\n${persona.nome},${persona.cognome},${persona.data_nascita}`;
-fs.writeFileSync('persona.csv', csvContent);
+function esporta(){
+    const nome = prompt('Inserisci il nome: ');
+    const cognome = prompt('Inserisci il cognome: ');
+    const data_nascita = prompt('Inserisci la data di nascita: ');
+    const persona = new Persona(nome, cognome, data_nascita);
+    console.log(persona.toString());
+    const csvContent = `nome,cognome,data_nascita\n${persona.nome},${persona.cognome},${persona.data_nascita}`;
+    fs.writeFileSync('persona.csv', csvContent);
+}
+
 
 // Importazione da formato CSV
-const csvImport = fs.readFileSync('persona.csv', 'utf8');
-const [header, data] = csvImport.split('\n');
-const [importedNome, importedCognome, importedDataNascita] = data.split(',');
-const importedPersona = new Persona(importedNome, importedCognome, importedDataNascita);
-console.log(importedPersona.toString());
+function importa(){
+    const csvImport = fs.readFileSync('persona.csv', 'utf8');
+    const [header, data] = csvImport.split('\n');
+    const [importedNome, importedCognome, importedDataNascita] = data.split(',');
+    const importedPersona = new Persona(importedNome, importedCognome, importedDataNascita);
+    console.log(importedPersona.toString());
+}
+
+while (true) {
+    console.log("Seleziona un'opzione:");
+    console.log("1. Importa un file CSV");
+    console.log("2. Esporta un file CSV");
+    console.log("0. Esci");
+    const choice = prompt("Scelta: ");
+    switch (choice) {
+      case "1":
+        importa();
+        break;
+      case "2":
+        esporta();
+        break;
+      case "0":
+        console.log("Programma terminato.");
+        process.exit(0);
+      default:
+        console.log("Scelta non valida.");
+    }
+  }
