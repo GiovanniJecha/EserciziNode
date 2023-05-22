15c1281905aa1a3b09394a26d74aa291c1c370d3
Giovanni Jecha <giovanni.jecha@studenti.isissgobetti.it>
2023-05-22 16:35:22 +0200

    test

--- Esercizio1/src/Verifica02.js
+++ Esercizio1/src/Verifica02.js
@@ -42,20 +42,51 @@ class Biblioteca {
     console.log("Seleziona un'opzione:");
     console.log("1. Crea una biblioteca");
     console.log("2. Aggiungi la lista libri a biblioteca esistente");
+    console.log("3. Cerca libro per titolo");
+    console.log("4. Trova libro piu economico");
     console.log("0. Esci");
     const choice = prompt("Scelta: ");
     switch (choice) {
       case "1":
-        let nomeBiblioteca = prompt("Inserisci il nome che vuoi dare alla biblioteca : ");
-        biblioteca = new Biblioteca(nomeBiblioteca);
-        console.log('Biblioteca "' + biblioteca.nome + '" creata.');
+        if (biblioteca != null){
+            let nomeBiblioteca = prompt("Inserisci il nome che vuoi dare alla biblioteca : ");
+            biblioteca = new Biblioteca(nomeBiblioteca);
+            console.log('Biblioteca "' + biblioteca.nome + '" creata.');
+        } else {
+            console.log("Devi prima creare la biblioteca!");
+        }
         break;
       case "2":
-        let nomeBibliotecaDaAggiungere = prompt("Inserisci il nome della biblioteca a cui aggiungere i libri: ");
-        if (nomeBibliotecaDaAggiungere == biblioteca.nome) {
-          importa();
+        if (biblioteca != null){
+            let nomeBibliotecaDaAggiungere = prompt("Inserisci il nome della biblioteca a cui aggiungere i libri: ");
+            if (nomeBibliotecaDaAggiungere == biblioteca.nome) {
+                importa();
+            } else {
+                console.log('Nessuna biblioteca trovata con il nome "' + nomeBibliotecaDaAggiungere + '".');
+            }
         } else {
-          console.log('Nessuna biblioteca trovata con il nome "' + nomeBibliotecaDaAggiungere + '".');
+            console.log("Devi prima creare la biblioteca!");
+        }
+        break;
+      case "3":
+        if (biblioteca != null){
+          let titolo = prompt("Inserisci il titolo del libro che vuoi cercare : ");
+          for(let i = 0; i < biblioteca.libri.length; i++){
+            if (biblioteca.libri[i].m_titolo == titolo){
+                console.log("Libro trovato!");
+                console.log(biblioteca.libri[i]);
+            }
+          }
+        } else {
+            console.log("Devi prima creare la biblioteca!");
+        }
+        break;
+      case "4":
+        if (biblioteca != null){
+            let libro_piu_economico = biblioteca.libri.reduce((min, libro) => libro.m_prezzo < min.m_prezzo ? libro : min);
+            console.log(libro_piu_economico);
+        } else {
+            console.log("Devi prima creare la biblioteca!");
         }
         break;
       case "0":
